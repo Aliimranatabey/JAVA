9e141588be5d02b990636b71a301222c3802f65e
Aliimranatabey <aliimranatabey@gmail.com>
2024-07-02 22:14:49 +0300

    hızlı_tekrar_baslangic

---
+++ firstProject.java
@@ -0,0 +1,9 @@
+public class firstProject{
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA");
+		System.out.print("1");
+		System.out.print("-2-");
+		System.out.println("3");
+		System.out.println("Welcome...");
+    }
+}
