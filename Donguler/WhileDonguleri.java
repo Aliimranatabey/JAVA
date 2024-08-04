4d025702a0b0a55ccaf656cc7a9a19aaffc0c0d8
Aliimranatabey <aliimranatabey@gmail.com>
2024-08-04 12:56:33 +0300

    while loops fast repeat

---
+++ Donguler/WhileDonguleri.java
@@ -0,0 +1,36 @@
+public class WhileDonguleri {
+    public static void main(String[] args) {
+        /*
+		  
+		  while(dongu kosulu){
+		  		// Kosul dogru oldugu surece calisir.
+		  }
+		  
+		 */
+
+		int i=0;
+		
+		//Aşağıdaki Kod Sonsuz Döngüye girmemize sebep olur
+		/*while(i<10) {
+			System.out.println("i : "+i);
+		}*/
+		
+		while(i<10) {
+			System.out.println("i : "+i);
+			i++;
+		}
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Bir sayı giriniz : ");
+		int number=scanner.nextInt();
+		int faktoriyel=1;
+		
+		while(number>0) {
+			faktoriyel=faktoriyel*number;
+			number--;
+		}
+		
+		System.out.println("Faktoriyel : "+faktoriyel);
+		
+    }
+}
