3cc6bf92b590e82361528038c43fc7c6034c82ee
Aliimranatabey <aliimranatabey@gmail.com>
2025-02-18 23:21:21 +0300

    kod blokları videosuna devam edildi

--- Meotdlar ve Metodlarda Overloading/KodBloklarindakiLokalDegiskenler.java
+++ Meotdlar ve Metodlarda Overloading/KodBloklarindakiLokalDegiskenler.java
@@ -1,6 +1,13 @@
 public class KodBloklarindakiLokalDegiskenler {
+    //Bir değişken kod bloğu içerisinde tanımlıdır kod bloğu bitince o değişken sonlanır yani bellekten silinir
     public static void main(String[] args) {
         int a=4;
         String yazdir="Merhaba";
+
+        if(a<10){
+            int b=5;
+            System.out.println(yazdir);
+        }
+        //System.out.println(b); !HATA VERIR
     }
 }
