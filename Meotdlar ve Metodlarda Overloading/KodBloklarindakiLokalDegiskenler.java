43ff35a46c969d84dc9f0eeb9ad6df3bd1fe33ec
Aliimranatabey <aliimranatabey@gmail.com>
2025-02-19 23:37:34 +0300

    local değişkenler videosu izlendi

--- Meotdlar ve Metodlarda Overloading/KodBloklarindakiLokalDegiskenler.java
+++ Meotdlar ve Metodlarda Overloading/KodBloklarindakiLokalDegiskenler.java
@@ -1,13 +1,23 @@
 public class KodBloklarindakiLokalDegiskenler {
+    public static void toplama(int a,int b,int c){
+        System.out.println(a+b+c);
+    }
     //Bir değişken kod bloğu içerisinde tanımlıdır kod bloğu bitince o değişken sonlanır yani bellekten silinir
     public static void main(String[] args) {
-        int a=4;
+        int t=5;
         String yazdir="Merhaba";
 
-        if(a<10){
-            int b=5;
+        if(t<10){
+            int k=5;
             System.out.println(yazdir);
         }
         //System.out.println(b); !HATA VERIR
+
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
+        toplama(3, 4, 6);
+       // a=6; //HATA VERIR
     }
 }
